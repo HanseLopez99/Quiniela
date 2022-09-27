128d86deac1102921409df8f2826e85339cf4a6b
Hansel López <halm29799@gmail.com>
2022-09-27 11:08:07 -0600

    generate player scaffold

---
+++ db/migrate/20220927170729_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :players do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
