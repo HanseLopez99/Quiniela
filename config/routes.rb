755d9fed635c4d111175c3263b744efe513a5549
Hansel López <halm29799@gmail.com>
2022-10-26 01:07:21 -0600

    feat: create API routes for team model

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,14 @@ Rails.application.routes.draw do
   # Route for scoreboard page
   get 'scoreboard', to: 'scoreboard#show'
 
+  # Routes for teams API
+  namespace :api do
+    resources :teams
+  end
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # devise_scope :user do
+  # devise_scope :user do  
   #   root 'devise/sessions#new'
   # end
 
